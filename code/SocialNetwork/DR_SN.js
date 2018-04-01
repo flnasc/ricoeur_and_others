baf4d932049b9acf836888299727a3f84b20aa2e
Ehdus9063 <ehdus9063@gmail.com>
2018-04-01 13:19:02 -0400

    date-based summarizatoin integrated into the graph

--- code/SocialNetwork/DR_SN.js
+++ code/SocialNetwork/DR_SN.js
@@ -12,6 +12,10 @@
 
 // Initialize the array for dataset
 var dataset = [];
+var barFiltered = [];
+var lineFiltered = [];
+var startYear = Number.MAX_SAFE_INTEGER;
+var endYear = Number.MIN_SAFE_INTEGER;
 
 // Read in the .csv file
 // Right now this reads in one file, but maybe we should expand it to read in all .csv files?
@@ -25,10 +29,16 @@ d3.csv("./data/summary.csv", function(error, data) {
     }
     else {
         console.log(data);
-
+        
         // Safety check that we are correctly reading in the file
         for (i = 0; i < dataset.length; i++) {
             console.log(dataset[i]);
+            if (dataset[i].Year < startYear) {
+                startYear = dataset[i].Year;
+            }
+            if (dataset[i].Year > endYear) {
+                endYear = dataset[i].Year;
+            }
         }
         // Call on the visualization function
     
@@ -42,6 +52,8 @@ d3.csv("./data/summary.csv", function(error, data) {
         // dataset.sort(sort_by('Frequency', true, parseInt));
         // // Ascending order
         // dataset.sort(sort_by('Frequency', flase, parseInt));
+        filterBar(barFiltered, startYear, endYear);
+
         barGraph();
     }
 });
@@ -57,7 +69,7 @@ function barGraph(){
     var height = 600;
     var padding = 80;
     var barPad = 1;
-    var barWidth = (width - 2 * padding) / dataset.length - barPad;
+    var barWidth = (width - 2 * padding) / barFiltered.length - barPad;
 
     // Create svg frame for visualization given the attributes above
     var svg = d3.select("body").
@@ -67,50 +79,50 @@ function barGraph(){
 
     // Set scales for x-axis and y-axis to afford dynamic scaling
     var xScale = d3.scaleBand().
-        domain(d3.range(dataset.length)).
+        domain(d3.range(barFiltered.length)).
         rangeRound([padding, width - padding]);
     var yScale = d3.scaleLinear().
-        domain([0,d3.max(dataset, function(d, i) {
-            return dataset[i].Frequency;
+        domain([0,d3.max(barFiltered, function(d, i) {
+            return barFiltered[i].Frequency;
        })]).
         range([height - padding, padding]);
     
     // Create the bar graph
     svg.selectAll("rect").
-        data(dataset).
+        data(barFiltered).
         enter().
         append("rect").
         attr("x", function(d,i) {
-            return (padding + i * ((width - 2 * padding) / dataset.length));}).
+            return (padding + i * ((width - 2 * padding) / barFiltered.length));}).
         attr("y", function(d, i) {
-            return height - (yScale(0) - yScale(dataset[i].Frequency) + padding)}).
+            return height - (yScale(0) - yScale(barFiltered[i].Frequency) + padding)}).
         attr("width", barWidth).
         attr("height", function(d, i) {
-            return yScale(0) - yScale(dataset[i].Frequency)}).
+            return yScale(0) - yScale(barFiltered[i].Frequency)}).
         attr("fill", "#191970");
    
     // Add frequency to the bar  
     var textSize = barWidth/3;
     svg.selectAll("text.labels").
-        data(dataset).
+        data(barFiltered).
         enter().
         append("text").
         text(function(d, i) {
-            return dataset[i].Frequency;}).
+            return barFiltered[i].Frequency;}).
         attr("x", function(d, i) {
-            return (barWidth/2 + padding + i * ((width - 2 * padding) / dataset.length));}).
+            return (barWidth/2 + padding + i * ((width - 2 * padding) / barFiltered.length));}).
         attr("y", function(d, i){ 
-            return (height - (yScale(0) - yScale(dataset[i].Frequency)) - padding)}).
+            return (height - (yScale(0) - yScale(barFiltered[i].Frequency)) - padding)}).
         attr("font-family", "arial").
         attr("font-size", textSize + "px").
         attr("fill", "red").
         style("text-anchor", "middle").
-        attr("class", "labels")
+        attr("class", "labels");
 
     // Define the axes
     var xAxis = d3.axisBottom(xScale).
         tickFormat(function(d, i) {
-            return dataset[i].Thinker;});
+            return barFiltered[i].Thinker;});
     var yAxis = d3.axisLeft(yScale);
 
     // Draw the axes
@@ -144,15 +156,67 @@ function barGraph(){
 // Helper function for sort
 // Borrowed from https://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-objects
 var sort_by = function(field, reverse, primer) {
+    
+    // Check for primer and see what additional steps must be made
     var key = primer ?
         function(x) {return primer(x[field])} :
         function(x) {return x[field]};
     
+    // Determines whether the directionality is reversed (1 or -1)    
     reverse = !reverse ? 1 : -1;
 
-    return function (a,b) {
+    // sort using reverse
+        return function (a,b) {
         return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
     }
 
 }
+
+// This function filters dataset to better accomodate for data being visualized
+// First, the dataset is filtered by the year range
+// Then, the frequency at which the thinker is refered to in Ricoeur's work
+// is summed up for each thinker so that we don't display multiple occasions for
+// each thinker
+function filterBar(barFiltered, startYear, endYEar) {
+
+    temp = [];
+
+    // If the datum falls into the date range, add it to the temporary arry
+    for (var i = 0; i < dataset.length; i++) {        
+        if (startYear <= dataset[i].Year && dataset[i].Year <= endYear) {
+            temp.push(dataset[i]);
+        }
+    }
+    // Sort the temporary array to make summation easier
+    temp.sort(sort_by('Thinker'), false, function(a) {return a});
+
+    // Initialize variables for summation
+    sum = 0;
+    bfLen = 0;
+
+    // Iterate through temp and look for redundancy in thinkers
+    for (var i = 0; i < temp.length; i++) {
+        // Add initial item
+        if (!i) {
+            barFiltered.push(temp[i]);
+            bfLen++;
+        } 
+        // If the current thinker is different from the previous one, 
+        // add the entry to the barFiltered array
+        else if (temp[i].Thinker != temp[i - 1].Thinker) {
+            barFiltered.push(temp[i]);
+            barFiltered[bfLen].Frequency = parseInt(temp[i].Frequency);
+            bfLen++;
+        } 
+        // If the current thinker has just been added to the array
+        // sum up frequency
+        else {
+            sum = parseInt(barFiltered[bfLen - 1].Frequency) + parseInt(temp[i].Frequency)
+            barFiltered[bfLen - 1].Frequency = sum;
+        }
+    }
+
+    delete temp;
+}
+
 
