16131e36b906a3ec38ffd3df54c4acf42c0d31fb
Alessandra Laurent <34074114+alealaurent@users.noreply.github.com>
2018-04-04 13:55:11 -0400

    remove _stanford from output filename

--- code/SocialNetwork/ExtractThinkers_Stanford.py
+++ code/SocialNetwork/ExtractThinkers_Stanford.py
@@ -66,7 +66,7 @@ def extract_network(file):
     #return agg_list
 
     #Write the list to a csv file with name in one column and frequency in the next
-    myfile = open(file[:-4] + '_stanford.csv', 'w', newline = '')#Name csv after the text file, removing .txt and adding .csv
+    myfile = open(file[:-4] + '.csv', 'w', newline = '')#Name csv after the text file, removing .txt and adding .csv
     with myfile:
         writer = csv.writer(myfile)
         writer.writerows(agg_list)
