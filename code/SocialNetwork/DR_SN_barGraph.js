d1041b49f1651552960a039fb451b69e309f01c3
Ehdus9063 <ehdus9063@gmail.com>
2018-05-21 16:20:09 -0400

    Separated the two graphs and allowing the users to search the thinkers on Digital Ricoeur website

--- code/SocialNetwork/DR_SN_barGraph.js
+++ code/SocialNetwork/DR_SN_barGraph.js
@@ -10,8 +10,6 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Variables that I think could be shared between the two graphs are defined here
 var startYear = Number.MAX_SAFE_INTEGER,
     refreshSY,
     endYear = Number.MIN_SAFE_INTEGER,
@@ -22,9 +20,7 @@ var startYear = Number.MAX_SAFE_INTEGER,
     height = 900,
     padding = 80,
     b_set = [],
-    barFiltered = [],
-    l_set = [];
-const l_default = 20;
+    barFiltered = [];
 
 // Read in the .csv file
 // Print out an error message if the file is unreadable.
@@ -66,60 +62,7 @@ function initialize() {
             checkReverse();
         }
     });
-
-    // Read in the csv file for line graph
-    // Similarly, check for error then initialize the graph
-    d3.csv("./data/lineAVG.csv", type, function(error, data) {
-        l_set = data;
-        if (error) {
-            console.log(error);
-            window.alert("Cannot read in the file.");
-        }
-        // console.log(l_set);
-        initLine();
-    });
-
-
-
-
 }
-
-function openCustom() {
-    var customDiv = document.getElementById("lineCustomize");
-    var displayStatus = customDiv.style.display;
-    if (document.getElementById("lineDefault").checked) {
-        customDiv.style.display = "none"; 
-        initLine();
-    }
-    else {
-        if(displayStatus=="none") {
-            customDiv.style.display = "table";
-        } else {
-            customDiv.style.display = "none";
-        }
-    }
-}
-
-function clearCustom() {
-    var i = 0;
-    while (document.getElementsByClassName("customCheck")[i]) {
-        document.getElementsByClassName("customCheck")[i].checked = false; 
-        i++;
-    }
-}
-
-function selectAllCustom() {
-    var i = 0;
-    while (document.getElementsByClassName("customCheck")[i]) {
-        document.getElementsByClassName("customCheck")[i].checked = true; 
-        i++;
-    }
-}
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// This part is associated with the bargraph for the time being
-//
 
 // The user can input the dates by hitting enter instead of clicking Date Range
 function checkInput() {
@@ -176,8 +119,6 @@ function resetDate() {
     sortData();
 }
 
-
-
 // From the two mutually exclusive radio buttons, fetch by which factor to sort barFiltered.
 // Then, sort barFiltered using updated sortBy and reverse, redrawing the bargraph
 function sortData() {
@@ -210,9 +151,7 @@ function checkReverse() {
             } else {
                 reverse = true;
             }
-
         }
-    
     }
 
     // console.log(reverse);
@@ -220,12 +159,9 @@ function checkReverse() {
 
 }
 
-
 // The main function for bargraph
 // Set dimension of the SVG, create the element, then draw the graph
-//
 function barGraph(){
-
 
     // Set dimensions for the visualization
     var barPad = 1;
@@ -243,8 +179,6 @@ function barGraph(){
         attr("width", width).
         attr("height", height).
         attr("id", "b_graph");
-
-    
 
     // Set scales for x-axis and y-axis to afford dynamic scaling
     var xScale = d3.scaleBand().
@@ -287,9 +221,15 @@ function barGraph(){
                 
         }).
         on("mouseout", function(d) {
+            // Make the tooltips invisible
             enlarged.transition().
                 duration(500).
                 style("opacity", 0);   
+        }).
+        on("click", function(d, i) {
+            // When the user clicks on the bar graph, then they are sent to the search page on Digital Ricoeur website
+            window.open("https://digitalricoeur.org/search/" + barFiltered[i].Thinker);
+            //window.location.replace("https://digitalricoeur.org/search/" + barFiltered[i].Thinker);
         });
    
     // Add frequency to the bar  
@@ -379,6 +319,7 @@ function wrapLabel(text, barWidth) {
         }
     });
 }
+
 // Helper function for sort
 // Borrowed from https://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-objects
 var sort_by = function(field, reverse, primer) {
@@ -395,7 +336,6 @@ var sort_by = function(field, reverse, primer) {
         return function (a,b) {
         return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
     }
-
 }
 
 // This function filters b_set to better accomodate for data being visualized
@@ -501,207 +441,6 @@ function filterBar() {
     document.getElementById("books").innerHTML = outNY;
 
     // console.log(barFiltered);
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// This part contains the functions used for the line chart
-//
-
-var thinkers_sh = [];
-// As of now, we are using a predfined color scheme with 20 colors.
-// We might need to define our own color scheme if we want to show more than 20 thinkers
-var l_x = d3.scaleTime().range([padding, width - padding]),
-    l_y = d3.scaleLinear().range([height - padding, padding]),
-    l_z = d3.scaleOrdinal(d3.schemeCategory20);
-
-// Using d3 library's timeParse function to parse years 
-var parseYear = d3.timeParse("%Y");
-
-// Draw line
-var line = d3.line().
-    x(function(d) {return l_x(d.year);}).
-    y(function(d) {return l_y(d.freq);});
-
-
-function initLine() {
-    // Create thinkers, which is a map that allows us to access individual frequencies,
-    // mapped onto each thinker and year
-    var thinkers = l_set.columns.slice(2).map(function(id) {
-        return {
-            id: id,
-            values: l_set.map(function(d) {
-                return {year: d.year, freq: d[id]};
-            })
-        };
-    });
-    //console.log(thinkers);
-    //console.log(thinkers[0]);
-    var customSize = document.getElementsByClassName("customThinkers").length;
-
-    for (var i = 0; i < customSize; i++) {
-        var rankedName = (i + 1).toString() + ". " + thinkers[i].id;
-        // console.log(rankedName);
-        document.getElementsByClassName("customThinkers")[i].innerHTML = rankedName;
-    }
-
-    // This part allows us to alternate between top 20 and custom of sort
-    var show;
-    if (document.getElementById("lineDefault").checked) {
-        d3.select("#l_graph").remove();
-        show = l_default;
-        // console.log("Drawing default lines");
-
-        // Empty thinkers_sh
-        thinkers_sh = [];
-        for (var i = 0; i < show; i++) {
-            thinkers_sh[i] = thinkers[i]; 
-        }
-    } else {
-        d3.select("#l_graph").remove();
-        // console.log("Drawing custom lines");
-
-        // Empty thinkers_sh
-        thinkers_sh = [];
-        for (var i = 0; i < customSize; i++) {
-            if (document.getElementsByClassName("customCheck")[i].checked) {
-                thinkers_sh.push(thinkers[i]); 
-            }
-        }
-    }
-
-    // define the domain for the axes
-    l_x.domain(d3.extent(l_set, function(d) {return d.year;}));
-    l_y.domain([
-        d3.min(thinkers_sh, function(c) {return d3.min(c.values, function(d) {return d.freq;}); }),
-        d3.max(thinkers_sh, function(c) {return d3.max(c.values, function(d) {return d.freq;}); })
-    ]);
-    l_z.domain(thinkers_sh.map(function(c) {return c.id;}));
-
-    // console.log(thinkers_sh);
-    drawLineChart();
-
-}
-
-
-// This function actually draws the line graph
-function drawLineChart() {
-
-
-    // The line graph will be appended to div of class "l_DIV"
-    var linegraph = d3.select("#l_DIV").
-        append("svg").
-        attr("class", "graph").
-        attr("width", width + padding).
-        attr("height", height).
-        attr("id", "l_graph");
-
-    var g = linegraph.append("g");
-
-    // Define axes for lin graph
-    var l_xAxis = d3.axisBottom(l_x);
-    var l_yAxis = d3.axisLeft(l_y);
-    // Draw the axes
-    g.append("g").
-        attr("class", "x axis").
-        attr("transform", "translate(0" + "," + (height - padding) + ")").
-        call(l_xAxis);
-    g.append("g").
-        attr("class", "y axis").
-        attr("transform", "translate(" + padding + ",0)").
-        call(l_yAxis);
-
-   // Label the axes     
-    g.append("text").
-        attr("transform", "translate(" + (width/2) + "," + (height - padding/2) + ")").
-        attr("font-weight", "bold").
-        text("Years");
-    g.append("text").
-        attr("transform", "rotate(-90)").
-        attr("y", padding / 3).
-        attr("x", 0 - height / 2).
-        attr("dy", "1em").
-        attr("font-weight", "bold").
-        style("text-anchor", "middle").
-        text("Frequency");
-
-    // Each thinker
-    var thinker = g.selectAll(".thinker").
-        data(thinkers_sh).
-        enter().
-        append("g").
-        attr("class", "thinker");
-
-    // For each thinker, draw a line using the values 
-    // Here, values defined in initLine function as thinkers was created
-    thinker.append("path").
-        attr("class", "line").
-        attr("id", function(d) { return d.id; }).
-        attr("d", function(d) {
-            return line(d.values); }).
-        style("stroke", function(d) { return l_z(d.id); }).
-        on("mouseover", function(d, i) {
-            // Appending the name of each thinker at the end of each line
-            thinker.append("text").
-                datum(function(d, i) { 
-                    // Don't use this right now, but we will want to use this in the future
-                }).
-                attr("transform", "translate("+ (width/2) + "," + padding + ")").
-                attr("x", 3).
-                attr("dy", "0.35em").
-                style("font", "20px sans-serif").
-                style("text-anchor", "middle").
-                text(this.id).
-                attr("id", "bigName");
-
-        }).
-        on("mouseout", function(d) {
-            // Clear all text 
-            thinker.select("#bigName").remove();
-        });
-
-
-    var fontSize = 15;
-    // Append the name of the thinker as the legend
-    thinker.append("text").
-        datum(function(d) {return {id: d.id, value: d.values[d.values.length - 1]};}).
-        attr("transform", function(d, i) {return "translate(" + l_x(d.value.year) + "," + (padding + i * fontSize) +")";}).
-        attr("x", fontSize).
-        attr("dy", "0.35em").
-        style("font", fontSize + "px sans-serif").
-        style("font-weight", "bold").
-        text(function(d) {return d.id;});
-  
-    // Append circles of appropriate color to the legend 
-    var legendOffset = 10;
-    var radius = 5;
-    thinker.append("circle").
-            datum(function(d) {return {id: d.id, value: d.values[d.values.length - 1]};}).
-            attr("transform", function(d, i) {return "translate(" + l_x(d.value.year) + "," + (padding + i * fontSize) + ")";}).
-            attr("cx", legendOffset).
-            attr("r", radius).
-            attr("fill", function(d, i) { return l_z(d.id);});
-    
-    // Initialize the tooltip to show Author's name 
-    var lineFreq = d3.select("#l_DIV").
-        append("div").
-        attr("class", "tooltip").
-        style("opacity", 0);
-}
-
-
-// When the csv file is read, all elements are read in as string
-// Here, we parse for date and convert the frequencies to numbers
-function type (d, _, columns) {
-    // Parse years into year format provided by d3 library
-    d.year = parseYear(d.year);
-    var n = columns.length;
-    for (var i = 1; i < n; ++i) {
-        c = columns[i];
-        d[c] = +d[c];
-    }
-    return d;
 }
 
 initialize();
